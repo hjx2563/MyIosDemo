c95ff3d9fb23a286d793cb928330ba726d3fc252
huangjinxin <huangjinxin@58.com>
2018-07-12 14:43:14 +0800

    Initial Commit

---
+++ MyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyApp
+//
+//  Created by huangjinxin on 2018/7/12.
+//  Copyright © 2018年 ___FUdLLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyApp
+//
+//  Created by huangjinxin on 2018/7/12.
+//  Copyright © 2018年 ___FUdLLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
